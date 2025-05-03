e41e36593a331d946d74e70aa1563eb561cf099b
KopralProject <kopralwann04@gmail.com>
2025-05-04 06:52:45 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -10,7 +10,7 @@ COLBG1="$(cat /etc/hokagevpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's
 ###########- END COLOR CODE -##########
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/hokagelegend9999/ijin/main/original > /root/tmp
+    curl -sS https://raw.githubusercontent.com/KopralProject/ijin/main/original > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -28,7 +28,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/hokagelegend9999/ijin/main/original | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/KopralProject/ijin/main/original | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -45,7 +45,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/hokagelegend9999/ijin/main/original | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/KopralProject/ijin/main/original | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -73,7 +73,7 @@ IP=$(curl -sS ipv4.icanhazip.com);
 date=$(date +"%Y-%m-%d")
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-NameUser=$(curl -sS https://raw.githubusercontent.com/hokagelegend9999/ijin/main/original | grep $MYIP | awk '{print $2}')
+NameUser=$(curl -sS https://raw.githubusercontent.com/KopralProject/ijin/main/original | grep $MYIP | awk '{print $2}')
 
 
 clear
@@ -109,7 +109,7 @@ zip -rP $InputPass $NameUser.zip backup > /dev/null 2>&1
 ##############++++++++++++++++++++++++#############
 LLatest=`date`
 Get_Data () {
-git clone https://raw.githubusercontent.com/hokagelegend9999/backup.git /root/user-backup/ &> /dev/null
+git clone https://raw.githubusercontent.com/KopralProject/backup.git /root/user-backup/ &> /dev/null
 }
 
 Mkdir_Data () {
@@ -136,8 +136,8 @@ Save_And_Exit () {
     git add . &> /dev/null
     git commit -m backup &> /dev/null
     git branch -M main &> /dev/null
-    git remote add origin https://raw.githubusercontent.com/hokagelegend9999/backup
-    git push -f https://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@github.com/hokagelegend9999/backup.git &> /dev/null
+    git remote add origin https://raw.githubusercontent.com/KopralProject/backup
+    git push -f https://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@github.com/KopralProject/backup.git &> /dev/null
 }
 
 if [ ! -d "/root/user-backup/" ]; then
@@ -152,7 +152,7 @@ sleep 1
 echo -e "$COLOR1│${NC}  [INFO] Processing updating server...... "
 Save_And_Exit
 fi
-link="https://raw.githubusercontent.com/hokagelegend9999/backup/main/$NameUser/$NameUser.zip"
+link="https://raw.githubusercontent.com/KopralProject/backup/main/$NameUser/$NameUser.zip"
 sleep 1
 echo -e "$COLOR1│${NC}  [INFO] Backup done "
 sleep 1
